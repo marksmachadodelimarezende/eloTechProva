bdece166274e8990cab673cf9b0b2835d78c58e9
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-07-31 17:48:07 -0300

    Create Backend Project

---
+++ scp/src/main/java/com/elotech/scp/SistemaCadastroPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.elotech.scp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCadastroPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCadastroPessoaApplication.class, args);
+	}
+
+}
---
+++ scp/src/test/java/com/elotech/scp/SistemaCadastroPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elotech.scp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
